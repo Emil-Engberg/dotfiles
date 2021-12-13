c43a930dd3f5c6ab71643bb34ec12fabc2419c8b
Emil Engberg <emil.engberg@outlook.com>
2021-12-13 04:16:56 +0100

    First version of install script.

--- install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-pacstrap /mnt linux linux-firmware base base-devel networkmanager emacs grub efibootmgr intel-ucode xorg xorg-xinit i3 lightdm lightdm-gtk3-greeter git pipewire pipewire-alsa pipewire-pulse brightnessctl 
-
-systemctl enable lightdm
