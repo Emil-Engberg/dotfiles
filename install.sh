793a5e24b19ce3cdefd3fc36c941871d462f0978
Emil Engberg <emil.engberg@outlook.com>
2023-02-02 20:11:01 +0100

    Improved disk partition.

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 umount -R /mnt
+sfdisk /dev/nvme0n1 < disk.txt
+mkfs.ext4 -q /dev/nvme0n1p2
+mkfs.fat -F 32 -q /dev/nvme0n1p1
 timedatectl set-ntp true
 rm -rf /etc/pacman.conf
 ln -sf /root/dotfiles/pacman.conf /etc/
 mount /dev/nvme0n1p2 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
-pacstrap /mnt base base-devel linux linux-firmware networkmanager emacs man-db man-pages texinfo amd-ucode grub efibootmgr nano xorg-server i3-gaps dmenu firefox gnome-terminal lightdm lightdm-gtk-greeter i3status neofetch git discord pipewire pipewire-alsa pipewire-pulse pavucontrol steam go bluez bluez-utils openssh bash-completion mesa lib32-mesa vulkan-intel
+pacstrap -K /mnt base base-devel linux linux-firmware networkmanager emacs man-db man-pages texinfo amd-ucode grub efibootmgr nano xorg-server i3-gaps dmenu firefox gnome-terminal lightdm lightdm-gtk-greeter i3status neofetch git discord pipewire pipewire-alsa pipewire-pulse pavucontrol steam go bluez bluez-utils openssh bash-completion mesa lib32-mesa vulkan-intel
 mount --mkdir /dev/sda1 /mnt/mnt/steam
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "root:$1" >> /mnt/pass.txt
